8a935dd74351b11be2b83f508ecfc6bbaac17327
mpalejko-source <acegrouppl@gmail.com>
2026-04-04 13:25:00 +0200

    Create wykresy.js

---
+++ data/wykresy.js
@@ -0,0 +1,8 @@
+window.ACE_WYKRESY = [
+  { title: "Pszenica", src: "assets/charts/pszenica.png" },
+  { title: "Kukurydza", src: "assets/charts/kukurydza.png" },
+  { title: "Rzepak", src: "assets/charts/rzepak.png" },
+  { title: "Pszenica – sezonowość", src: "assets/charts/pszenica-2.png" },
+  { title: "Kukurydza – sezonowość", src: "assets/charts/kukurydza-2.png" },
+  { title: "Rzepak – sezonowość", src: "assets/charts/rzepak-2.png" }
+];
