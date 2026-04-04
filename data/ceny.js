d2b3729c7ed2f5377edad33514a398e47322c555
mpalejko-source <acegrouppl@gmail.com>
2026-04-04 13:00:14 +0200

    Create ceny.js

---
+++ data/ceny.js
@@ -0,0 +1,41 @@
+window.ACE_CENY = {
+  title: "Ceny na wczorajsze zamknięcie",
+  dateLabel: "Stan na: 03.04.2026",
+  items: [
+    {
+      name: "Pszenica konsumpcyjna",
+      market: "Polska",
+      value: "915 zł/t",
+      week: "+12 zł/t",
+      month: "+28 zł/t"
+    },
+    {
+      name: "Kukurydza",
+      market: "Polska",
+      value: "848 zł/t",
+      week: "-6 zł/t",
+      month: "+14 zł/t"
+    },
+    {
+      name: "Rzepak",
+      market: "Polska",
+      value: "2 164 zł/t",
+      week: "+35 zł/t",
+      month: "+92 zł/t"
+    },
+    {
+      name: "MATIF Pszenica",
+      market: "Euronext",
+      value: "228,50 €/t",
+      week: "+2,75 €/t",
+      month: "+7,00 €/t"
+    },
+    {
+      name: "MATIF Rzepak",
+      market: "Euronext",
+      value: "482,25 €/t",
+      week: "-4,00 €/t",
+      month: "+11,75 €/t"
+    }
+  ]
+};
