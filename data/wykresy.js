d2d5d5b7dba7821dd5b985cab9590278d418beb2
mpalejko-source <acegrouppl@gmail.com>
2026-04-07 12:31:51 +0200

    Update wykresy.js

--- data/wykresy.js
+++ data/wykresy.js
@@ -4,5 +4,9 @@ window.ACE_WYKRESY = [
   { title: "Rzepak", src: "assets/charts/rzepak.png" },
   { title: "Pszenica – sezonowość", src: "assets/charts/pszenica-2.png" },
   { title: "Kukurydza – sezonowość", src: "assets/charts/kukurydza-2.png" },
-  { title: "Rzepak – sezonowość", src: "assets/charts/rzepak-2.png" }
+  { title: "Rzepak – sezonowość", src: "assets/charts/rzepak-2.png" },
+  { title: "Śruta sojowa", src: "assets/charts/sruta-sojowa.png" },
+  { title: "Śruta rzepakowa", src: "assets/charts/sruta-rzepakowa.png" },
+  { title: "EUR/PLN", src: "assets/charts/eurpln.png" },
+  { title: "USD/PLN", src: "assets/charts/usdpln.png" }
 ];
