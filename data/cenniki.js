7321e07458e73d929554f775f9510ad28d945e2f
mpalejko-source <acegrouppl@gmail.com>
2026-04-07 12:40:34 +0200

    Create cenniki.js

---
+++ data/cenniki.js
@@ -0,0 +1,114 @@
+window.ACE_CENNIKI = {
+  dateTitle: "Ceny: 03.04.2026",
+  poland: [
+    {
+      name: "Pszenica konsumpcyjna",
+      market: "Polska",
+      value: "915 zł/t",
+      week: "+12 zł/t",
+      month: "+28 zł/t"
+    },
+    {
+      name: "Pszenica paszowa",
+      market: "Polska",
+      value: "875 zł/t",
+      week: "+8 zł/t",
+      month: "+19 zł/t"
+    },
+    {
+      name: "Kukurydza",
+      market: "Polska",
+      value: "848 zł/t",
+      week: "-6 zł/t",
+      month: "+14 zł/t"
+    },
+    {
+      name: "Rzepak",
+      market: "Polska",
+      value: "2 164 zł/t",
+      week: "+35 zł/t",
+      month: "+92 zł/t"
+    },
+    {
+      name: "Śruta sojowa",
+      market: "PL port Gdynia",
+      value: "1 865 zł/t",
+      week: "-15 zł/t",
+      month: "-42 zł/t"
+    },
+    {
+      name: "Śruta rzepakowa",
+      market: "PL centralna Polska",
+      value: "1 185 zł/t",
+      week: "+5 zł/t",
+      month: "+24 zł/t"
+    }
+  ],
+  global: [
+    {
+      name: "MATIF Pszenica",
+      market: "Euronext",
+      value: "228,50 €/t",
+      week: "+2,75 €/t",
+      month: "+7,00 €/t"
+    },
+    {
+      name: "MATIF Kukurydza",
+      market: "Euronext",
+      value: "214,75 €/t",
+      week: "+1,25 €/t",
+      month: "+5,00 €/t"
+    },
+    {
+      name: "MATIF Rzepak",
+      market: "Euronext",
+      value: "482,25 €/t",
+      week: "-4,00 €/t",
+      month: "+11,75 €/t"
+    },
+    {
+      name: "CBOT Pszenica",
+      market: "Chicago",
+      value: "563,50 c/bu",
+      week: "+14,25 c/bu",
+      month: "+31,00 c/bu"
+    },
+    {
+      name: "CBOT Kukurydza",
+      market: "Chicago",
+      value: "452,25 c/bu",
+      week: "+8,50 c/bu",
+      month: "+21,00 c/bu"
+    },
+    {
+      name: "CBOT Soja",
+      market: "Chicago",
+      value: "1 038,75 c/bu",
+      week: "-9,25 c/bu",
+      month: "+18,50 c/bu"
+    }
+  ],
+  fx: [
+    {
+      name: "EUR/PLN",
+      market: "FX",
+      value: "4,3120",
+      week: "+0,0240",
+      month: "+0,0510"
+    },
+    {
+      name: "USD/PLN",
+      market: "FX",
+      value: "3,9785",
+      week: "-0,0135",
+      month: "+0,0820"
+    },
+    {
+      name: "EUR/USD",
+      market: "FX",
+      value: "1,0838",
+      week: "+0,0045",
+      month: "-0,0090"
+    }
+  ]
+};
